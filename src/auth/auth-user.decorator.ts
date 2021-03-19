2c76556e858a8bb4a854ecd71cf7c19651ca0c68
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-03-19 20:30:38 +0900

    edit auth user decorator

--- src/auth/auth-user.decorator.ts
+++ src/auth/auth-user.decorator.ts
@@ -3,8 +3,11 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const AuthUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
+    console.log('Context', context);
     const gqlContext = GqlExecutionContext.create(context).getContext();
+    console.log('gqlContext', gqlContext);
     const user = gqlContext['user'];
+    console.log(user);
     return user;
   },
 );
